09de745e57018a8966a2c4af7562cff6939e3a29
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-11-13 17:05:38 +0100

    Added basic CRUD actions to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
+  def index
+    render json: 'Index action'
+  end
+
+  def show
+    render json: 'Show action'
+  end
+
+  def create
+    render json: 'Create action'
+  end
+
+  def update
+    render json: 'Update action'
+  end
+
+  def destroy
+    render json: 'Delete action'
+  end
 end
