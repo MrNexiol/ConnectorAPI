202b1edbdecb445176a4d6a46d1e536beee225c7
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-11-19 13:34:46 +0100

    Added error message when user list is empty

--- app/services/gist_contact_extractor.rb
+++ app/services/gist_contact_extractor.rb
@@ -8,18 +8,18 @@ class GistContactExtractor
     next_page_token: nil,
     next_sync_token: nil
   }
+  @error_template = {
+    error_code: nil,
+    error_message: nil
+  }
 
   def self.parse_contact_list(json)
     ruby_res = MultiJson.load(json)
-    tmp = []
-    ruby_res['contacts'].each do |contact|
-      tmp2 = contact.select do |k, _v|
-        KEYS_TO_EXTRACT_LIST.include? k
-      end
-      tmp << tmp2
+    if ruby_res['contacts'].empty?
+      no_contacts_error
+    else
+      json_contact_list ruby_res
     end
-    @json_template['contacts'] = tmp
-    MultiJson.dump(@json_template)
   end
 
   def self.parse_contact(json)
@@ -32,4 +32,24 @@ class GistContactExtractor
     end
     MultiJson.dump(tmp)
   end
+end
+
+private
+
+def json_contact_list(ruby_response)
+  tmp = []
+  ruby_response['contacts'].each do |contact|
+    tmp2 = contact.select do |k, _v|
+      KEYS_TO_EXTRACT_LIST.include? k
+    end
+    tmp << tmp2
+  end
+  @json_template['contacts'] = tmp
+  MultiJson.dump(@json_template)
+end
+
+def no_contacts_error
+  @error_template['error_code'] = 204
+  @error_template['error_message'] = 'No contacts found'
+  MultiJson.dump(@error_template)
 end
