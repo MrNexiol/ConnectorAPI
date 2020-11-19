c900ef35689a782e108132b09dee063e8f3815a1
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-11-19 14:01:49 +0100

    Added error code for not found contact

--- app/services/gist_contact_extractor.rb
+++ app/services/gist_contact_extractor.rb
@@ -15,7 +15,7 @@ class GistContactExtractor
 
   def self.parse_contact_list(json)
     ruby_res = MultiJson.load(json)
-    if ruby_res['contacts'].empty?
+    if ruby_res.key? 'errors'
       no_contacts_error
     else
       json_contact_list ruby_res
@@ -24,13 +24,11 @@ class GistContactExtractor
 
   def self.parse_contact(json)
     ruby_res = MultiJson.load(json)
-    tmp = ruby_res
-    unless ruby_res['errors']
-      tmp = ruby_res['contact'].select do |k, _v|
-        KEYS_TO_EXTRACT.include? k
-      end
+    if ruby_res.key? 'errors'
+      no_contact_error
+    else
+      json_contact ruby_res
     end
-    MultiJson.dump(tmp)
   end
 end
 
@@ -40,7 +38,7 @@ def json_contact_list(ruby_response)
   tmp = []
   ruby_response['contacts'].each do |contact|
     tmp2 = contact.select do |k, _v|
-      KEYS_TO_EXTRACT_LIST.include? k
+      self::KEYS_TO_EXTRACT_LIST.include? k
     end
     tmp << tmp2
   end
@@ -48,8 +46,21 @@ def json_contact_list(ruby_response)
   MultiJson.dump(@json_template)
 end
 
+def json_contact(ruby_response)
+  tmp = ruby_response['contact'].select do |k, _v|
+    self::KEYS_TO_EXTRACT.include? k
+  end
+  MultiJson.dump(tmp)
+end
+
 def no_contacts_error
   @error_template['error_code'] = 204
   @error_template['error_message'] = 'No contacts found'
+  MultiJson.dump(@error_template)
+end
+
+def no_contact_error
+  @error_template['error_code'] = 204
+  @error_template['error_message'] = 'No contact with given id found'
   MultiJson.dump(@error_template)
 end
