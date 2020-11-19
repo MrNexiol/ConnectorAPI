6e1aad90789a7110029cac3de2e10237b6204816
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-11-19 22:53:54 +0100

    Added parsing of next and last page of contacts, refactored some duplicated code

--- app/services/gist_contact_extractor.rb
+++ app/services/gist_contact_extractor.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class GistContactExtractor
-  KEYS_TO_EXTRACT_LIST = %w[id full_name job_title company_name avatar].freeze
   KEYS_TO_EXTRACT = %w[id phone_number first_name last_name job_title company_name mobile_phone_number avatar].freeze
   @json_list_template = {
     contacts: nil,
@@ -11,6 +10,12 @@ class GistContactExtractor
   @error_template = {
     error_code: nil,
     error_message: nil
+  }
+  @error_codes = {
+    parameter_missing: [204, 'Contact identifier missing'],
+    not_present: [400, 'Contact email missing'],
+    not_found: [204, 'No contact with given id found'],
+    internal_error: [500, 'Internal server error']
   }
 
   def self.parse_contact_list(json)
@@ -38,18 +43,12 @@ def json_contact_list(ruby_response)
   tmp = []
   ruby_response['contacts'].each do |contact|
     tmp2 = contact.select do |k, _v|
-      self::KEYS_TO_EXTRACT_LIST.include? k
+      self::KEYS_TO_EXTRACT.include? k
     end
     tmp << tmp2
   end
-  prepare_response ruby_response, tmp
+  prepare_response ruby_response['pages'], tmp
   MultiJson.dump(@json_list_template)
-end
-
-def prepare_response(ruby_response, tmp)
-  @json_list_template['next_page'] = Rack::Utils.parse_query(URI(ruby_response['pages']['next']).query)['page']
-  @json_list_template['last_page'] = Rack::Utils.parse_query(URI(ruby_response['pages']['last']).query)['page']
-  @json_list_template['contacts'] = tmp
 end
 
 def json_contact(ruby_response)
@@ -59,44 +58,23 @@ def json_contact(ruby_response)
   MultiJson.dump(tmp)
 end
 
-def choose_error_message(errors)
-  if errors['code'] == 'parameter_missing'
-    identifier_missing_error
-  elsif errors['code'] == 'not_present'
-    email_missing_error
-  elsif errors['code'] == 'not_found'
-    no_contact_error
-  else
-    internal_server_error
+def prepare_response(ruby_response, tmp)
+  if ruby_response['next']
+    @json_list_template['next_page'] = Rack::Utils.parse_query(URI(ruby_response['next']).query)['page']
+    @json_list_template['last_page'] = Rack::Utils.parse_query(URI(ruby_response['last']).query)['page']
   end
+  @json_list_template['contacts'] = tmp
+end
+
+def choose_error_message(errors)
+  tmp = errors['code'].to_sym
+  @error_template['error_code'] = @error_codes[tmp][0]
+  @error_template['error_message'] = @error_codes[tmp][1]
+  MultiJson.dump(@error_template)
 end
 
 def no_contacts_error
   @error_template['error_code'] = 204
   @error_template['error_message'] = 'No contacts found'
-  MultiJson.dump(@error_template)
-end
-
-def no_contact_error
-  @error_template['error_code'] = 204
-  @error_template['error_message'] = 'No contact with given id found'
-  MultiJson.dump(@error_template)
-end
-
-def identifier_missing_error
-  @error_template['error_code'] = 400
-  @error_template['error_message'] = 'Contact identifier missing'
-  MultiJson.dump(@error_template)
-end
-
-def email_missing_error
-  @error_template['error_code'] = 400
-  @error_template['error_message'] = 'Contact email missing'
-  MultiJson.dump(@error_template)
-end
-
-def internal_server_error
-  @error_template['error_code'] = 500
-  @error_template['error_message'] = 'Internal server error'
   MultiJson.dump(@error_template)
 end
