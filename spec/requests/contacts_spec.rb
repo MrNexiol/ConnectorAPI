fe446f17a308b1a59f2f8d2bb0ce182da04a9ebc
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-11-20 09:49:45 +0100

    Added show action tests and more index action tests

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -3,16 +3,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Contacts', type: :request do
-  context 'tests' do
-    it 'requests contact list' do
+  context 'index action' do
+    it 'renders contact list' do
       get contacts_path
-      expect(response.body).to include 'contacts'
+      res = MultiJson.load(response.body)
+      expect(res['contacts']).not_to be_empty
     end
 
-    it 'should render single contact' do
-      get '/contacts/1'
+    it 'renders contacts list with specified number of contacts' do
+      get contacts_path(params: { per_page: 1 })
       res = MultiJson.load(response.body)
-      expect(res['errors'][0]['message']).to eql "Couldn't find Contacts"
+      expect(res['contacts'].length).to eql 1
+    end
+  end
+
+  context 'show action' do
+    let!(:correct_id) { 212_312_933 }
+    let!(:unused_id) { 1 }
+    let!(:wrong_type_id) { 'asd' }
+
+    it 'renders error on unused id' do
+      get contact_path(unused_id)
+      res = MultiJson.load(response.body)
+      expect(res['error_message']).to eql 'No contact with given id found'
+    end
+
+    it 'renders error on wrong type id' do
+      get contact_path(wrong_type_id)
+      res = MultiJson.load(response.body)
+      expect(res['error_message']).to eql 'No contact with given id found'
+    end
+
+    it 'renders contact data when id found' do
+      get contact_path(correct_id)
+      res = MultiJson.load(response.body)
+      expect(res).to include 'company_name'
     end
   end
 end
